1fc4edb342034c057c7cd21b84a85918a9fed036
Raffaele Mignone <raffaele.mignone@pm.me>
2018-10-23 19:30:17 +0200

    add SwingFactory
    - add SwingFactory
    - add AgendaSwing
    - add GroupsSwing
    - add LoginSwing
    - add StudentsSwing

---
+++ src/main/kotlin/views/swing/GroupsSwing.java
@@ -0,0 +1,29 @@
+package views.swing;
+
+import Util.MessageType;
+import controllers.GroupsController;
+import model.Model;
+import org.jetbrains.annotations.NotNull;
+import views.GroupsView;
+
+public class GroupsSwing implements GroupsView {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void setController(@NotNull GroupsController controller) {
+
+    }
+
+    @Override
+    public void setModel(@NotNull Model model) {
+
+    }
+
+    @Override
+    public void showMessage(@NotNull String message, @NotNull MessageType type) {
+
+    }
+}
---
+++ src/main/kotlin/views/swing/LoginSwing.java
@@ -0,0 +1,29 @@
+package views.swing;
+
+import Util.MessageType;
+import controllers.LoginController;
+import model.Model;
+import org.jetbrains.annotations.NotNull;
+import views.LoginView;
+
+public class LoginSwing implements LoginView {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void setController(@NotNull LoginController controller) {
+
+    }
+
+    @Override
+    public void setModel(@NotNull Model model) {
+
+    }
+
+    @Override
+    public void showMessage(@NotNull String message, @NotNull MessageType type) {
+
+    }
+}
