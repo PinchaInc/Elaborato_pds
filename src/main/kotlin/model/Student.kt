1021b08b6e76ed0f08b0aace9107c018f2ae9f59
Raffaele Mignone <raffaele.mignone@pm.me>
2018-10-26 20:00:18 +0200

    add guard on set group and course

--- src/main/kotlin/model/Student.kt
+++ src/main/kotlin/model/Student.kt
@@ -2,5 +2,16 @@ package model
 
 data class Student(val name: String, val surname: String, val id: Int) {
     var group: Group? = null
+        set(g) {
+            if (field == null)
+                field = g
+        }
     var course: Course? = null
+        set(c) {
+            if (field != c) {
+                field?.removeStudent(this)
+                field = c
+                field?.addStudent(this)
+            }
+        }
 }
