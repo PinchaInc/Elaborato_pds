dc7d7ee27f828130a55ddf0d02776b448a3ad0e1
norangebit <git@norangeb.it>
2018-11-01 14:54:21 +0100

    edit controllers
    - edit addReview to GroupsController
    - add startAgenda, startGroups and startStudents to LoginControllers

--- src/main/kotlin/controllers/GroupsController.kt
+++ src/main/kotlin/controllers/GroupsController.kt
@@ -1,11 +1,9 @@
 package controllers
 
-import model.Review
-
 interface GroupsController {
     fun start()
     fun startStudents()
     fun startAgenda()
     fun assignWork(groupID: Int, workTrackID: Int)
-    fun addReview(groupID: Int, meetingID: Int, review: Review)
+    fun addReview(groupID: Int, meetingID: Int, reviewTitle: String, reviewBody: String, reviewRating: Int? = null)
 }
--- src/main/kotlin/controllers/LoginController.kt
+++ src/main/kotlin/controllers/LoginController.kt
@@ -2,5 +2,8 @@ package controllers
 
 interface LoginController {
     fun start()
+    fun startAgenda()
+    fun startGroups()
+    fun startStudents()
     fun authenticate(username: String, password: String)
 }
