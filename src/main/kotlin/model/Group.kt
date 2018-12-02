9fc46e3e477b5d4b0a4499716064324862d36b9b
norangebit <git@norangeb.it>
2018-12-02 15:13:17 +0100

    fix

--- src/main/kotlin/model/Group.kt
+++ src/main/kotlin/model/Group.kt
@@ -1,7 +1,7 @@
 package model
 
 class Group(var name: String, vararg students: Student) {
-    var group: Group? = null
+    var course: Course? = null
     private val members = ArrayList<Student>()
     private val meetings = ArrayList<Meeting>()
     var work: Work? = null
