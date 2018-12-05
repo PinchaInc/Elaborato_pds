dc74742173df9d4598fcd31a93b7645b8116d745
norangebit <git@norangeb.it>
2018-12-05 21:52:21 +0100

    better implementation of connection

--- src/main/kotlin/repository/sqlite/Connection.kt
+++ src/main/kotlin/repository/sqlite/Connection.kt
@@ -4,16 +4,23 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 object Connection : repository.Connection {
+    private val url = "jdbc:sqlite:../../../Scaricati/sync/pds.db"
+    private var conn: Connection
+
+    init {
+        conn = setConnection()
+    }
+
+    fun setConnection(): Connection {
+        val conn = DriverManager.getConnection(url)
+        val stm = conn.prepareStatement("PRAGMA foreign_keys = on")
+        stm.execute()
+        return conn
+    }
 
     override fun getConnection(): Connection {
-        val url = "jdbc:sqlite:../../../Scaricati/sync/pds.db"
-        val conn = lazy {
-            val conn = DriverManager.getConnection(url)
-            val stm = conn.prepareStatement("PRAGMA foreign_keys = on")
-            stm.execute()
-            conn
-        }
-
-        return conn.value
+        if (conn.isClosed)
+            conn = setConnection()
+        return conn
     }
 }
